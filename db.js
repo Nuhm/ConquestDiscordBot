38be0bf5558b427b92aace7945af68332d3411cf
Mikey <52801181+Nuhm@users.noreply.github.com>
2023-10-29 18:01:00 +0000

    Update db.js

--- db.js
+++ db.js
@@ -1,9 +1,9 @@
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-   host: '213.239.212.247',
+   host: 'localhost',
    user: 'root',
-   password: '123Cheese',
+   password: 'password',
    database: 'unturned_sr',
 });
 
